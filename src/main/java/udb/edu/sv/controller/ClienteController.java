0910641f9b1cbdad0df0e4c626f0e4ffd2d495f7
Paola <pao.fer009@gmail.com>
2024-05-16 17:38:26 -0600

    Funcionalidad de Compra y Recibo

--- src/main/java/udb/edu/sv/controller/ClienteController.java
+++ src/main/java/udb/edu/sv/controller/ClienteController.java
@@ -1,15 +1,19 @@
 package udb.edu.sv.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import udb.edu.sv.dao.UsuarioDAO;
 import udb.edu.sv.dao.model.Compra;
+import udb.edu.sv.dao.model.Evento_Boleteria;
+import udb.edu.sv.dao.model.Usuario;
 import udb.edu.sv.service.CompraService;
 import udb.edu.sv.service.Evento_BoleteriaService;
-import udb.edu.sv.service.UsuarioService;
 
 @Controller
 public class ClienteController
@@ -18,16 +22,25 @@ public class ClienteController
     private CompraService compraService;
 
     @Autowired
-    private UsuarioService usuarioService;
+    private Evento_BoleteriaService eventoBoleteriaService;
 
     @Autowired
-    private Evento_BoleteriaService eventoBoleteriaService;
+    private UsuarioDAO usuarioDAO;
+
 
     @GetMapping("/comprarEvento/{id}")
     public String comprarEvento(@PathVariable("id") Long id, Model model) {
         if (id != null && id != 0) {
-            model.addAttribute("usuarios", usuarioService.getAll());
-            model.addAttribute("compra", new Compra());
+            Evento_Boleteria boleteria = eventoBoleteriaService.get(id);
+            Compra compra = new Compra();
+            compra.setEvento(boleteria.getEvento());
+
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            String email = authentication.getName();
+            Usuario usuario = usuarioDAO.findByEmail(email);
+            compra.setUsuario(usuario);
+
+            model.addAttribute("compra", compra);
             model.addAttribute("boleteria", eventoBoleteriaService.get(id));
             return "cliente/HacerCompra";
         } else {
